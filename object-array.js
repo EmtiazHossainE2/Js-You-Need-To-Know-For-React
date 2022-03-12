b54efc7e3b8b4c3dfec9d7888cbb0ec6d773a4b8
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-12 23:58:18 +0600

    async await concept

--- object-array.js
+++ object-array.js
@@ -132,3 +132,22 @@ const object2 = {
 };
 
 console.log(Object.values(object2));  output: Array ["somestring", 42, false] */
+
+//7 An async function is a function declared with the async keyword, and the await keyword is permitted within it. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains. 
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+}
+
+asyncCall();
+
