19eab6b86c2211a9a9dfaacf07ef57a2cf880fdc
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-13 23:30:57 +0600

    rest parameter

--- jaavscript-revision-practice/app.js
+++ jaavscript-revision-practice/app.js
@@ -1,10 +1,10 @@
 var name = "hero alom";
-var lname = "Uganda";
+var country = "Uganda";
 //আমরা  যদি  সাধারণ  ভাবে  করি  তাহলে এভাবে  করতে   পারি
-console.log("Hello " + name + " Welcome to " + lname);
+console.log("Hello " + name + " Welcome to " + country);
 
 //এবং  খুব  সহজে  টেমপ্লেট স্ট্রিং  বাববার  করে  যে  কোনো  জায়গায়  ডাইনামিক  ভ্যালু  ব্যবহার  করতে   পারি
-console.log(`Hello, ${name}!Welcome to ${lname}`);
+console.log(`Hello, ${name}!Welcome to ${country}`);
 
 // spread operators can be used for arrays or objects
 
