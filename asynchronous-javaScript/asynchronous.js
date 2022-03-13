6e1febbd516fd3ce4c1fcea04b3f91168842a324
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-13 19:16:51 +0600

    synchronous function

---
+++ asynchronous-javaScript/asynchronous.js
@@ -0,0 +1,29 @@
+function makeGreeting(name) {
+    return `Hello, my name is ${name}!`;
+}
+
+const name = 'Miriam';
+const greeting = makeGreeting(name);
+console.log(greeting);
+// "Hello, my name is Miriam!"
+//Here makeGreeting() is a synchronous function, because the caller has to wait for the function to finish its work before the function returns.
+
+
+
+// An async function is a function declared with the async keyword, and the await keyword is permitted within it. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains. 
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+}
+
+asyncCall();
