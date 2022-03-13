2a3e983c4260ee01d9a7750c1a0bd2576d7d30ab
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-13 15:41:22 +0600

    callback js

---
+++ recap-js/recap.js
@@ -0,0 +1,17 @@
+var price = 33
+
+var name = 'Shabana'
+
+var boxName = 'Cocola';
+
+var price_88 = 34;
+
+var eNum = -1;
+
+var _$box78 = 'Monika';
+
+var home_address = "kochu khet"
+
+var nums = [1, 5, 8]
+console.log(nums.indexOf(0));  // 0 ki ace nah so return korbe -1 
+console.log(nums.indexOf(1));  // 1 ki ace ,ha , return korbe position ta .programming language e position suru hoy 0 theke .1 jehetu sobar age tai 0 return korce . indexOf diye ekta array te value ace kina check kora hoy 
